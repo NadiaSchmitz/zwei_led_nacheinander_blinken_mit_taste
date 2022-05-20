ece13f3ead2c21bc4e480826ee1b24cdf82648b4
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-05-20 14:32:19 +0200

    20052022

--- zwei_led_nacheinander_blinken_mit_taste.ino
+++ zwei_led_nacheinander_blinken_mit_taste.ino
@@ -1,23 +1,23 @@
-int led_red = 8;
+int led_blue = 8;
 int led_green = 13;
 int button = 7;
 
 void setup() {
-  pinMode(led_red,OUTPUT);
+  pinMode(led_blue,OUTPUT);
   pinMode(led_green,OUTPUT);
   pinMode(button, INPUT_PULLUP);
 }
 
 void loop() {
-  if (digitalRead(button) == HIGH) {
-    digitalWrite(led_red,HIGH);
+  if (digitalRead(button) == LOW) {
+    digitalWrite(led_blue,HIGH);
     digitalWrite(led_green,LOW);
     delay(500);
-    digitalWrite(led_red,LOW);
+    digitalWrite(led_blue,LOW);
     digitalWrite(led_green,HIGH);
     delay(500);
   } else {
     digitalWrite(led_green,LOW);
-    digitalWrite(led_red,LOW);
+    digitalWrite(led_blue,LOW);
   }
 }
